eca69dddcc69c94817a1a550c47c99b5a87ffb95
jkjgfdsfdghb <69359690+ilyadash@users.noreply.github.com>
2022-06-05 22:03:13 +0300

    Changing output directories

--- program/src/Constants.cpp
+++ program/src/Constants.cpp
@@ -1,7 +1,7 @@
-const double MeV=1;
-const double keV=0.001;
-const double eV=10e-6;
-const double u=931.4940954;//аем
-const double Mp=938.2720813;
-const double Mn=939.5654133;
-const double Me=0.5109989461;
+const double MeV=1;//MeV
+const double keV=0.001;//MeV
+const double eV=10e-6;//MeV
+const double u=931.4940954;//атомная единица массы в MeV
+const double Mp=938.2720813;//MeV
+const double Mn=939.5654133;//MeV
+const double Me=0.5109989461;//MeV
