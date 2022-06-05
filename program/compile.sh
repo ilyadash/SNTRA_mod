d83525fb82368161797108085b2796514b734770
jkjgfdsfdghb <69359690+ilyadash@users.noreply.github.com>
2022-06-05 21:17:51 +0300

    Changed program directories
    Made src directory.

--- program/compile.sh
+++ program/compile.sh
@@ -1,2 +1,2 @@
-g++ -g -Wl,--no-as-needed `root-config --cflags` `root-config --libs` -o SNTRA SNTRA.cpp
-cp SNTRA ../
+g++ -g -Wl,--no-as-needed `root-config --cflags` `root-config --libs` -o SNTRA src/SNTRA.cpp
+#cp SNTRA ../
