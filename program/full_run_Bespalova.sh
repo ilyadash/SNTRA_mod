6a9cd592054503ad4cdcf4cde5ca9c9879a5cc32
Ilya <di261297@rambler.ru>
2022-07-08 15:14:33 +0300

    Create full_run_Bespalova.sh
    For auto run on all Bespalova Dataset

---
+++ program/full_run_Bespalova.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+./compile.sh
+./SNTRA ../Bespalova/28Si/Neutron/ txt
+./SNTRA ../Bespalova/30Si/Neutron/ txt
+./SNTRA ../Bespalova/28Si/Proton/ txt
+./SNTRA ../Bespalova/30Si/Proton/ txt
