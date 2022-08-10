19aca2ea7ac30cdb1507cf15eaa01e2a0c25b9d5
Ilya <di261297@rambler.ru>
2022-08-10 13:59:29 +0300

    Create S_run_TT.sh

---
+++ program/S_run_TT.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+./compile.sh
+./SNTRA ../data_sets/TT/32S/Neutron/ txt output/TT
+./SNTRA ../data_sets/TT/34S/Neutron/ txt output/TT
+./SNTRA ../data_sets/TT/32S/Proton/ txt output/TT
+./SNTRA ../data_sets/TT/34S/Proton/ txt output/TT
